6a0204142f66825bb1d71b269e641fb7eb809bbb
Kazato Sugimoto <uiureo@gmail.com>
2012-12-13 11:38:34 +0900

    add a canvas simulator feature

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,4 +1,66 @@
+// gif simulator on canvas
+var GifSim = (function(){
+  var that = {};
+
+  var canvas, ctx;
+  var images = [], delay, current = 0, timer;
+
+  that.init = function () {
+    canvas = $('#gif_sim')[0];
+    ctx = canvas.getContext("2d");
+    that.start();
+  };
+
+  that.start = function () {
+    if (images.length === 0) {
+      return;
+    }
+
+    timer = setInterval(function () {
+      if (current >= images.length) {
+        current = 0;
+      }
+
+      var image = images[0]
+        , larger = _.max([image.width, image.height]);
+
+      if (image.width > image.height) {
+        ctx.drawImage(images[current], 0, 0, canvas.width, image.height * (canvas.height / larger));
+      } else {
+        ctx.drawImage(images[current], 0, 0, image.width * (canvas.width / larger), canvas.height);
+      }
+
+      current++;
+    }, delay * 10);
+  };
+
+  that.reset = function () {
+    clearInterval(timer);
+  };
+
+  that.update = function (args) {
+    that.reset();
+
+    that.updateImages(args.images);
+    delay = args.delay || 10;
+
+    that.start();
+  };
+
+  that.updateImages = function (srcs) {
+    images = srcs.map(function (src) {
+      var image = new Image();
+      image.src = src;
+      return image;
+    });
+  };
+
+  return that;
+}());
+
 $(function () {
+  GifSim.init();
+
   var image_template = _.template($('#image_template').text());
 
   $.get('/api/thumbs.json')
@@ -21,6 +83,8 @@ $(function () {
       selected_images.push(src);
       $image.addClass('image_selected');
     }
+
+    GifSim.update({ images: selected_images, delay: $gif_delay.val() });
 
     $('#debug').text(selected_images.join(', '));
   });
@@ -52,5 +116,9 @@ $(function () {
   $gif_delay_value.text($gif_delay.val() * 10);
   $gif_delay.change(_.throttle(function () {
     $gif_delay_value.text($gif_delay.val() * 10);
+  }, 100));
+
+  $gif_delay.change(_.debounce(function () {
+    GifSim.update({ images: selected_images, delay: $gif_delay.val() });
   }, 100));
 });
